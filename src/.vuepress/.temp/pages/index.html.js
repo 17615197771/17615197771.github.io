1820a254c62bce9640e4d90c98893a92863cb1fd
chenyucheng <3479084822@qq.com>
2022-12-03 01:33:11 +0800

    123

---
+++ src/.vuepress/.temp/pages/index.html.js
@@ -0,0 +1,14 @@
+export const data = JSON.parse("{\"key\":\"v-8daa1a0e\",\"path\":\"/\",\"title\":\"小鱼号\",\"lang\":\"en-US\",\"frontmatter\":{\"home\":true,\"layout\":\"Blog\",\"icon\":\"home\",\"title\":\"小鱼号\",\"heroImage\":\"/logo.svg\",\"heroText\":\"小鱼号\",\"tagline\":\"东师大俩傻孩子的故事\",\"heroFullScreen\":true,\"projects\":[{\"icon\":\"陈\",\"name\":\"弧形折叠空间\",\"desc\":\"找到陈\",\"link\":\"https://user.qzone.qq.com/3479084822/infocenter\"},{\"icon\":\"孙\",\"name\":\".的QQ空间\",\"desc\":\"找到孙\",\"link\":\"https://user.qzone.qq.com/3296785939\"},{\"icon\":\"book\",\"name\":\"book name\",\"desc\":\"Detailed description of the book\",\"link\":\"https://link.to.your.book\"},{\"icon\":\"article\",\"name\":\"article name\",\"desc\":\"Detailed description of the article\",\"link\":\"https://link.to.your.article\"},{\"icon\":\"friend\",\"name\":\"friend name\",\"desc\":\"Detailed description of friend\",\"link\":\"https://link.to.your.friend\"},{\"icon\":\"/logo.svg\",\"name\":\"custom item\",\"desc\":\"Detailed description of this custom item\",\"link\":\"https://link.to.your.friend\"}],\"footer\":\"customize your footer text\",\"description\":\"This is a blog home page demo. To use this layout, you should set both layout: Blog and home: true in the page front matter. For related configuration docs, please see blog home...\",\"head\":[[\"meta\",{\"property\":\"og:url\",\"content\":\"https://mister-hope.github.io/\"}],[\"meta\",{\"property\":\"og:site_name\",\"content\":\"Blog Demo\"}],[\"meta\",{\"property\":\"og:title\",\"content\":\"小鱼号\"}],[\"meta\",{\"property\":\"og:description\",\"content\":\"This is a blog home page demo. To use this layout, you should set both layout: Blog and home: true in the page front matter. For related configuration docs, please see blog home...\"}],[\"meta\",{\"property\":\"og:type\",\"content\":\"website\"}],[\"meta\",{\"property\":\"og:locale\",\"content\":\"en-US\"}],[\"meta\",{\"property\":\"og:locale:alternate\",\"content\":\"zh-CN\"}],[\"link\",{\"rel\":\"alternate\",\"hreflang\":\"zh-cn\",\"href\":\"https://mister-hope.github.io/zh/\"}]]},\"excerpt\":\"\",\"headers\":[],\"readingTime\":{\"minutes\":0.56,\"words\":167},\"autoDesc\":true,\"filePathRelative\":\"README.md\"}")
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updatePageData) {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ data }) => {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  })
+}
---
+++ src/README.md
@@ -0,0 +1,48 @@
+---
+home: true
+layout: Blog
+icon: home
+title: 小鱼号
+heroImage: /logo.svg
+heroText: 小鱼号
+tagline: 东师大俩傻孩子的故事
+heroFullScreen: true
+projects:
+  - icon: 陈
+    name: 弧形折叠空间
+    desc: 找到陈
+    link: https://user.qzone.qq.com/3479084822/infocenter
+
+  - icon: 孙
+    name: .的QQ空间
+    desc: 找到孙
+    link: https://user.qzone.qq.com/3296785939
+
+  - icon: book
+    name: book name
+    desc: Detailed description of the book
+    link: https://link.to.your.book
+
+  - icon: article
+    name: article name
+    desc: Detailed description of the article
+    link: https://link.to.your.article
+
+  - icon: friend
+    name: friend name
+    desc: Detailed description of friend
+    link: https://link.to.your.friend
+
+  - icon: /logo.svg
+    name: custom item
+    desc: Detailed description of this custom item
+    link: https://link.to.your.friend
+
+footer: customize your footer text
+---
+
+This is a blog home page demo.
+
+To use this layout, you should set both `layout: Blog` and `home: true` in the page front matter.
+
+For related configuration docs, please see [blog homepage](https://vuepress-theme-hope.github.io/v2/guide/blog/home/).
